5db377e07ea4a9fa770e4853f0efd4284f401fc3
Bill Tutt <bill@tutts.org>
2012-08-03 01:25:48 -0400

    Revert "__eq__ methods should compare the underlying data if the hashes are identical."
    This reverts commit 0236f606c56f0e13d6fd5fd9ef5e7e70400595d7.

--- snakeguice/annotation.py
+++ snakeguice/annotation.py
@@ -8,9 +8,7 @@ class Annotation(object):
         return hash(self.__class__.__name__ + ":" + self._value)
 
     def __eq__(self, other):
-        return self.__hash__() == other.__hash__() \
-               and self.__class__.__name__ == other.__class__.__name__ \
-               and self._value == other._value
+        return self.__hash__() == other.__hash__()
 
     def __ne__(self, other):
         return not self == other
