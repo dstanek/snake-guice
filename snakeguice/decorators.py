4ba7151b7f173c9ee6b2cc4992f8dbe9e33f9dd5
David Stanek <dstanek@dstanek.com>
2022-07-18 00:37:08 -0400

    refactor: removing GuiceData/Arg
    A step toward simplifying the code base.

--- snakeguice/decorators.py
+++ snakeguice/decorators.py
@@ -2,54 +2,6 @@ import inspect
 import sys
 
 
-class GuiceData:
-    def __init__(self):
-        self.init = None
-        self.methods = {}
-
-    @classmethod
-    def from_class(cls, target_class):
-        if "__guice__" not in target_class.__dict__:
-            guice_data = cls()
-            try:
-                target_class.__guice__ = guice_data
-            except TypeError:  # special case for builtin/extension types
-                return guice_data
-        return target_class.__guice__
-
-    @classmethod
-    def composite_from_class(cls, target_class):
-        composite_data = GuiceData()
-        for _cls in target_class.__mro__[-1::-1]:
-            data = GuiceData.from_class(_cls)
-            if data.init:  # only the last class in the chain
-                composite_data.init = data.init
-            for name, method in data.methods.items():
-                composite_data.methods[name] = method
-
-        return composite_data
-
-    @classmethod
-    def from_class_dict(cls, class_dict):
-        if "__guice__" not in class_dict:
-            class_dict["__guice__"] = cls()
-        return class_dict["__guice__"]
-
-
-class GuiceArg:
-    def __init__(self, datatype=None, annotation=None, scope=None):
-        self.datatype = datatype
-        self.annotation = annotation
-        self.scope = scope
-
-    def __eq__(self, other):
-        return (self.datatype, self.annotation, self.scope) == (
-            other.datatype,
-            other.annotation,
-            other.scope,
-        )
-
-
 def _validate_method_args(method, kwargs):
     """Validate decorator args when used to decorate a method."""
     sig = inspect.signature(method)
@@ -73,19 +25,12 @@ def inject(**kwargs):
     #     del kwargs["scope"]
 
     def _inject(method):
+        method.__guice_types__ = kwargs
         class_locals = enclosing_frame().f_locals
 
-        guice_data = GuiceData.from_class_dict(class_locals)
-
-        annotations = getattr(method, "__guice_annotations__", {})
-
-        gmethod = dict((k, GuiceArg(v, annotations.get(k))) for k, v in kwargs.items())
-
         _validate_method_args(method, kwargs)
-        if method.__name__ == "__init__":
-            guice_data.init = gmethod
-        else:
-            guice_data.methods[method.__name__] = gmethod
+        if method.__name__ != "__init__":
+            class_locals.setdefault("__guice_methods__", set()).add(method.__name__)
 
         return method
 
